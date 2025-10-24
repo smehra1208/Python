d73c37ffffe3ae4eae0d78091dbfaf4cb2a3d70f
Sakshi Mehra <smehra1208@gmail.com>
2025-10-24 15:29:33 +0530

    Update 24 Oct'25 Python Practice.py

--- Daily Python Practice/24 Oct'25 Python Practice.py
+++ Daily Python Practice/24 Oct'25 Python Practice.py
@@ -1,31 +1,31 @@
 #1. WAP Area of Rectangle
-length = int(input("Enter length of the rectangle: "))
-breadth = int(input("Enter breadth of the rectangle: "))
+length = flaot(input("Enter length of the rectangle: "))
+breadth = float(input("Enter breadth of the rectangle: "))
 Area = length*breadth
 print("Area of rectangle is", Area)
 
 #2. WAP Area of Triangle
-base = int(input("Enter base of the triangle: "))
-height= int(input("Enter height of the triangle: "))
+base = float(input("Enter base of the triangle: "))
+height= float(input("Enter height of the triangle: "))
 Area = (1/2)*base*height
 print("Area of triangle is", Area)
 
 #3. WAP Area of Trapezium
-a = int(input("Enter length of first side: "))
-b = int(input("Enter length of second side: "))
+a = float(input("Enter length of first side: "))
+b = float(input("Enter length of second side: "))
 height= int(input("Enter height of the trapezium: "))
 Area = (1/2)*(a+b)*height
 print("Area of trapezium is", Area)
 
 #4. WAP Area of Circle
 import math
-radius = int(input("Enter radius of circle: "))
+radius = float(input("Enter radius of circle: "))
 Area = math.pi*radius**2
 print("Area of circle is", Area)
 
 #5. WAP Convert kilometers to miles
 # 1km = 0.621 miles
-km = int(input("Enter the distance in km: "))
+km = float(input("Enter the distance in km: "))
 miles = km*0.621
 print("Distance is", miles, "miles")
 
@@ -39,10 +39,17 @@ Where:
 • a = Acceleration
 '''
 
-v= int(input("Enter the Final velocity: "))
-u= int(input("Enter the Initial velocity: "))
-a= int(input("Enter the Acceleration: "))
+v= float(input("Enter the Final velocity: "))
+u= float(input("Enter the Initial velocity: "))
+a= float(input("Enter the Acceleration: "))
 d= (v**2-u**2)/(2*a)
 print("Displacement is", d)
+
+#7. WAP Surface Area of a Cuboid
+length = float(input("Enter length of cuboid: "))
+breadth = float(input("Enter breadth of cuboid: "))
+height = float(input("Enter height of cuboid: "))
+surface_area = 2*((length*breadth)+(breadth*height)+(height*length))
+print("Surface area of cuboid is", surface_area)
 
 
