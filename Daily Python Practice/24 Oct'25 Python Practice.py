b866d2206d3589f2608a1c2b36f2a9990b4d47bf
Sakshi Mehra <smehra1208@gmail.com>
2025-10-24 15:11:47 +0530

    Create 24 Oct'25 Python Practice.py

---
+++ Daily Python Practice/24 Oct'25 Python Practice.py
@@ -0,0 +1,18 @@
+#1. WAP Area of rectangle
+length = int(input("Enter length of the rectangle: "))
+breadth = int(input("Enter breadth of the rectangle: "))
+Area = length*breadth
+print("Area of rectangle is", Area)
+
+#2. WAP Area of Triangle
+base = int(input("Enter base of the triangle: "))
+height= int(input("Enter height of the triangle: "))
+Area = (1/2)*base*height
+print("Area of triangle is", Area)
+
+#3. WAP Area of Trapezium
+a = int(input("Enter length of first side: "))
+b = int(input("Enter length of second side: "))
+height= int(input("Enter height of the trapezium: "))
+Area = (1/2)*(a+b)*height
+print("Area of trapezium is", Area)
